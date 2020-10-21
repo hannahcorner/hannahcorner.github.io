06aef50611da9f7685ff1558081cf95a8d915879
Hannah Corner <hannahcorner15@gmail.com>
2020-10-20 22:40:23 -0500

    add next button

--- scripts/case.js
+++ scripts/case.js
@@ -7,3 +7,24 @@ function closeNav() {
     document.getElementById("sideNavigation").style.width = "0";
     document.getElementById('sideNavigation').setAttribute('aria-hidden', 'true');
 }
+
+// Get the modal
+var modal = document.getElementById("imgModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("projectimg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
